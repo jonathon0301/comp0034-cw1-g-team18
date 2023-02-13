db914061635164da79590ce4e9e80c7d9b4c1c3c
JonathonShi <71091633+JonathonShi@users.noreply.github.com>
2023-02-13 07:51:03 +0000

    Testing

--- test/test_gender_pay_gap_dash_app/test_gender_pay_gap_dash_app.py
+++ test/test_gender_pay_gap_dash_app/test_gender_pay_gap_dash_app.py
@@ -1,17 +1,47 @@
-import pytest
+import time
+from basepage import BasePage
 from selenium import webdriver
-
-@pytest.fixture
-def browser():
-    browser = webdriver.Safari()
-    yield browser
-    browser.close()
+from selenium.webdriver.common.by import By
+driver = BasePage(webdriver.Chrome(executable_path='/comp0034-cw1-g-team18/test/chromedriver_mac_arm64/chromedriver'))
 
 
-def test_example(browser):
-    # Launch the web app
-    browser.get("http://localhost:8050/")
+driver.get("http://localhost:8050/")
+time.sleep(15)
+fruit_s = driver.find_elements(By.CSS_SELECTOR, "#histogram-selector label input")
+for fruiti in fruit_s:
+    fruiti.click()
 
-    # Verify the presence of certain elements on the page
-    element = browser.find_element_by_id("h1")
-    assert element == "Gender Pay Gap Visualization"
+driver.find_element(By.CSS_SELECTOR, '#xaxis_selector').click()
+for i in range(1, 4):
+    time.sleep(2)
+    a = '.VirtualizedSelectOption:nth-child({})'.format(i)
+    driver.find_element(By.CSS_SELECTOR, a).click()
+    time.sleep(1)
+    driver.find_element(By.CSS_SELECTOR, '#yaxis_selector').click()
+    for j in range(1, 4):
+        time.sleep(1)
+        b = '.VirtualizedSelectOption:nth-child({})'.format(j)
+        driver.find_element(By.CSS_SELECTOR, b).click()
+        time.sleep(1)
+        driver.find_element(By.CSS_SELECTOR, '#yaxis_selector').click()
+    time.sleep(1)
+    driver.find_element(By.CSS_SELECTOR, '#xaxis_selector').click()
+
+driver.find_element(By.CSS_SELECTOR, '#map_selector').click()
+for c1 in range(1, 4):
+    time.sleep(2)
+    c = '.VirtualizedSelectOption:nth-child({})'.format(c1)
+    driver.find_element(By.CSS_SELECTOR, c).click()
+    time.sleep(1)
+    driver.find_element(By.CSS_SELECTOR, '#map_selector').click()
+
+driver.find_element(By.CSS_SELECTOR, '#bar_selector').click()
+for d1 in range(1, 4):
+    time.sleep(2)
+    d = '.VirtualizedSelectOption:nth-child({})'.format(d1)
+    driver.find_element(By.CSS_SELECTOR, d).click()
+    time.sleep(1)
+    driver.find_element(By.CSS_SELECTOR, '#bar_selector').click()
+
+# Quit Browser
+driver.quit()
